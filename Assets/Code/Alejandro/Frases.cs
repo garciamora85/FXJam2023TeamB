0d96f7bb2a74cf0bc01fbda4064bc49e40782899
Juan José Gil López <jjgill@esimurcia.com>
2023-07-18 16:55:23 +0200

    Frases puestas

--- Assets/Code/Alejandro/Frases.cs
+++ Assets/Code/Alejandro/Frases.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public static class Frases
 {
     private static string[] _frasesAlmacenadas = new string[] {
-        "a",
-        "aaaaaaaaaaaaaa",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" };
+        "Mi corazon... Duele...",
+        "Ay no, creo que me esta dando un ataque.",
+        "Dentro del pecho hay algo que hace bom bom...",
+        "La sangre deberia estar dentro de mi cuerpo, ¿no?",
+        "¡Mi pierna!, ¡¡¡AAAAAHHHH!!!",
+        "So... co... rroooo....",
+        "O tengo hambre o algo me esta devorando por dentro.",
+        "Creo que tengo un xenomorfo... deberia haber usado proteccion.",
+        "¡Mi pecho!, ¡Dolor!",
+        "Hay una viga en mi...",
+        "No es por echarle mas hierro al asunto pero, ayuda...",
+        "No puedo moverme, llamena  una ambulancia o algo.",
+        "Que mareo, me han subido la factura de la luz.",
+        "Hola buenas, ¿me puedes dar un paracetamol?",
+        "Yo estoy sano, pero una revision siempre viene bien.",
+        "¡EY, NO, LLEVAME A MI!",
+        "¡YO ESTOY MAS GRAVE!",
+        "Yo soy mejor que el, ¡Salvame!"
+
+    };
 
     public static string GetFrase(int n)
     {
