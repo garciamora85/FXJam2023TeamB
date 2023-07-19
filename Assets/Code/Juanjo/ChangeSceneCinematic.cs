734d6d6545e0d169fd7e3a5745f1051231cff05c
Juan José Gil López <jjgill@esimurcia.com>
2023-07-19 17:19:50 +0200

    Colliders FINAL

---
+++ Assets/Code/Juanjo/ChangeSceneCinematic.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneCinematic : MonoBehaviour
+{
+    float time;
+    public float timeMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime;
+        if (time >= timeMax)
+        {
+            SceneManager.LoadScene("Principal");
+        }
+    }
+}
